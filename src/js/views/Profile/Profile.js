b92cb2f4b15d847f077b94dceb88c69838e212db
Jesus Moreno <jmoreno91@alumnos.uaq.mx>
2023-12-07 17:15:01 -0600

    Ya terminé settings y Profile

--- src/js/views/Profile/Profile.js
+++ src/js/views/Profile/Profile.js
@@ -4,23 +4,23 @@ import './profile.css'
 
 export default function Profile() {
     return (
-        <div>
+        <div className="screen">
             <Header id_usuario={0}/>
-            <div class="screen">
-                <div class="edit-profile">
+            <div className="screen3">
+                <div className="edit-profile">
                     <h1>Mi cuenta</h1>
-                    <div class="edition">
-                        <div class="edit">
-                            <div class="changes" ><input id="newEmail" type="text" placeholder="Cambiar correo" /></div>
-                            <div class="changes"><input id="newName" type="text" placeholder="Cambiar nombre" /></div>
-                            <div class="changes"><input id="newPassword" type="password" placeholder="Cambiar contraseña" /></div>
-                            <div class="changes"><button Onclick="">Guardar cambios</button></div>                           
+                    <div className="edition">
+                        <div className="edit">
+                            <div className="changes" ><input id="newEmail" type="text" placeholder="Cambiar correo" /></div>
+                            <div className="changes"><input id="newName" type="text" placeholder="Cambiar nombre" /></div>
+                            <div className="changes"><input id="newPassword" type="password" placeholder="Cambiar contraseña" /></div>
+                            <div className="changes"><button Onclick="">Guardar cambios</button></div>                           
                         </div>
-                        <div class="photo">
+                        <div className="photo">
                             <img src="assets/images/usuario.png" />
                         </div>
                     </div>
-                    <div class="eliminar">
+                    <div className="eliminar">
                         <button>Eliminar cuenta</button>
                     </div>
                 </div>
--- src/js/views/Setting/Setting.js
+++ src/js/views/Setting/Setting.js
@@ -1,13 +1,30 @@
 import React from 'react'
 import Header from '../../components/shared/header/Header'
+import './setting.css'
 
 export default function Setting() {
     return (
-        <div>
+        <div className="screen">
             <Header id_usuario={0}/>
-            <div>
-                Configuración
-            </div>
+                <div className="screen2">
+                    <div className="settings">
+                        <h1>Configuración</h1>
+                        <div className="options">
+                            <div className="configure">Cambiar pregunta de seguridad </div>
+                            <div className="configure">
+                                Notificaciones 
+                                <select>
+                                    <option>Activadas</option>
+                                    <option>Desactivadas</option>
+                                </select>
+                            </div>
+                            
+                            <div className="configure">Ayuda </div>
+                            <div className="configure">Política de privacidad </div>
+                            <div className="configure">Condiciones de servicio </div>
+                        </div>
+                    </div>
+                </div>
         </div>
     )
 }
